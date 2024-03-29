f069e2e2e573aec7e40f8d4ae06899da49e84cdb
robfalken <hello@robfalken.co>
2024-03-29 20:01:49 +0100

    Add some basic styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Rob Falken.</main>;
+  return (
+    <main className="flex items-center p-2">
+      <Image
+        src="/robfalken.png"
+        alt="Rob Falken"
+        width={32}
+        height={32}
+        className="rounded mr-2"
+      />
+      Rob Falken.
+    </main>
+  );
 }
