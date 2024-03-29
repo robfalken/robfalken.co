59738bea8d43d1004c2bb6cf2a8c0cec5f5a482c
robfalken <hello@robfalken.co>
2024-03-29 20:20:03 +0100

    Implement MDX support

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,15 @@
+import createWithMDX from "@next/mdx";
+
+const withMDX = createWithMDX();
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+export default withMDX({
+  // Configure `pageExtensions` to include MDX files
+  pageExtensions: ["mdx", "tsx"],
+  // Optionally, add any other Next.js config below
   output: "export",
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
-};
-
-export default nextConfig;
+});
